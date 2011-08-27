ff768264e806557f4a8cb5f2eb0ad5a7431f9ca9
Matt MacDonald <matt@prx.org>
2011-08-27 12:31:57 -0400

    Added pdftotext and open calais

--- town-council-parse.rb
+++ town-council-parse.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'typhoeus'
 require 'nokogiri'
 require 'uri'
+require 'json'
+require 'calais'
 
 BASE_URL = 'http://www.ci.watertown.ma.us'
 
@@ -27,10 +29,39 @@ begin
     
       file_url = URI.escape(document_link)
       puts "DOWNLOADING: " + file_url
-      system("curl -O "+file_url)
+      system("mkdir town-council-minutes")
+      system("cd town-council-minutes > /dev/null ; curl -O "+file_url)
     end
   end
 
 rescue Nokogiri::XML::SyntaxError => e
   puts "caught exception: #{e}"
 end
+
+# convert all the PDFs to text
+Dir.chdir("town-council-mintes")
+files = Dir.glob("*.pdf")
+
+files.each do |f|
+  system("pdftotext "+f)
+end
+
+# now send each of the PDFs through Open Calais
+Dir.chdir("town-council-mintes")
+files = Dir.glob("*.txt")
+
+files.each do |f|
+  result = Calais.enlighten(:content => content, :content_type = :raw, output_format => :json, :license_id = "r6q88uv3d2pwjfrsr4e8d9j5")
+  
+  # write the json result out to files
+  File.open(f+".json", 'w') {|f| f.write(result.to_s) }
+end
+
+
+
+#ruby-1.9.2-p180 :051 > quotes.each do |q|
+#ruby-1.9.2-p180 :052 >     name = Siren.query "$[q['person']]", parsed
+#ruby-1.9.2-p180 :053?>   puts "FROM: "+ name
+#ruby-1.9.2-p180 :054?>   puts "QUOTE: "+ q["quote"]
+#ruby-1.9.2-p180 :055?>   end
+
